e4c093767d3ffc585331bd20c4dc0a5939f52876
Neven Cvetkovic <nevenc@pivotal.io>
2019-08-21 14:17:43 +0200

    Initial commit.

---
+++ src/main/java/com/example/cities/CitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesServiceApplication.class, args);
+	}
+
+}
