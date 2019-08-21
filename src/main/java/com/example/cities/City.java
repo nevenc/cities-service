3065cd95d1f9532f782b4e12b7b05102aeffc5d4
Neven Cvetkovic <nevenc@pivotal.io>
2019-08-21 16:54:28 +0200

    Updated for Mongo backing database (removed JPA/SQL)

--- src/main/java/com/example/cities/City.java
+++ src/main/java/com/example/cities/City.java
@@ -3,20 +3,15 @@ package com.example.cities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class City {
 
     @Id
-    @GeneratedValue
-    private Long id;
+    private String id;
 
     private String name;
     private String country;
--- src/main/java/com/example/cities/CityRepository.java
+++ src/main/java/com/example/cities/CityRepository.java
@@ -1,10 +1,6 @@
 package com.example.cities;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
-public interface CityRepository extends JpaRepository<City,Long> {
-    List<City> findAllByNameContaining(@Param("nameContains") String query);
+public interface CityRepository extends MongoRepository<City,String> {
 }
